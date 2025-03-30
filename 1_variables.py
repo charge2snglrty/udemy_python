00bd0455c17f8b42eec40b949515f8728c54ea43
moon <26782747+charge2snglrty@users.noreply.github.com>
2025-03-30 18:49:00 +0000

    commenting the vairbles filee

---
+++ 1_variables.py
@@ -0,0 +1,17 @@
+# in this file we are going to swa-p
+
+
+a = 10 
+b = 25 
+print("Before swapping")
+print("a =", a)
+print("b =", b)
+# swapping
+temp = a
+a = b
+b = temp
+print("After swapping")
+print("a =", a)
+print("b =", b)
+# or a and b
+# swapping      
